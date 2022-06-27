f6e761b9a3eef00116b22765d3ae5d420ed6cf8b
unknown <Ksamboni>
2022-06-27 11:03:13 -0500

    Se agrega el Proyecto

---
+++ build/default/debug/_ext/152413541/CERRADURA_2.o.d
@@ -0,0 +1,8 @@
+build/default/debug/_ext/152413541/CERRADURA_2.o:  \
+	C:/Users/Asus/Documents/DIGITALES\ II/TECLADO_09/CERRADURA_2.ASM  \
+	C:/Users/Asus/Documents/DIGITALES\ II/TECLADO_09/TECLADO.INC  \
+	C:/Users/Asus/Documents/DIGITALES\ II/TECLADO_09/LCD_4BIT.INC  \
+	C:/Users/Asus/Documents/DIGITALES\ II/TECLADO_09/LCD_MENS.INC  \
+	C:/Users/Asus/Documents/DIGITALES\ II/TECLADO_09/RETARDOS.INC  \
+	C:/Users/Asus/Documents/DIGITALES\ II/TECLADO_09/EEPROM.INC  \
+
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=CERRADURA_ELECTRONICA_3.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/CERRADURA_ELECTRONICA_3.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=cerraduraelectronica3.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/cerraduraelectronica3.x.tar
